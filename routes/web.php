2b5d084aeb59ed376dc4f2c270c2f45fe6dffb20
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-08-09 10:00:15 +0530

    set_template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin/index');
 });
